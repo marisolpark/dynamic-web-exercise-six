2014fee3ad15b5cd2edc2751ebc8c8d842bdf05c
Marisol Park <marisolpark@10-18-174-175.dynapool.wireless.nyu.edu>
2023-11-27 19:03:08 -0500

    deploy

--- src/app/components/CreateUserForm.js
+++ src/app/components/CreateUserForm.js
@@ -1,7 +1,20 @@
+import styles from "./components.module.css";
 const CreateUserForm = () => {
     return(
         <div>
             <h2>Create User Form</h2>
+            <form className={styles.Form}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" />
+                 
+                <button type="submit">Create User</button>
+            </form>
         </div>
     );
 };
--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,7 +1,19 @@
+import styles from "./components.module.css"
+
 const LoginForm = () => {
     return(
         <div>
             <h2>LoginForm</h2>
+            <form className={styles.Form}>
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" />
+                 
+                <button type="submit">Create User</button>
+            </form>
         </div>
     );
 };
